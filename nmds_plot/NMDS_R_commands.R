509fd328908863ed3db8e6dc725a13e8116d2270
Kevin Myers <kevinmyers@WEI00249.local>
2023-03-27 09:51:40 -0500

    updating commands

--- nmds_plot/NMDS_R_commands.R
+++ nmds_plot/NMDS_R_commands.R
@@ -15,6 +15,8 @@ data <- read.table(file = "rel_abund_allMAGS.txt", header = TRUE, sep = "\t")
 data_melt <- reshape::melt(data, id.vars=c("Rel_Abundance"))
 write.table(data_melt, file = "rel_abundance_allMAGS_long_from_R.txt", sep = "\t")
 
+#edit file manually in Excel
+
 relData <- read.delim(file = "rel_abundance_allMAGS_long_from_R_for_NMDS.txt", header = TRUE, sep = "\t")
 str(relData)
 head(relData)
@@ -43,24 +45,6 @@ nmds_data <- as.data.frame(nmds$points)
 nmds_data$Sample_Name <- plot_Data$Sample_Name
 nmds_data <- dplyr::left_join(nmds_data, plot_Data)
 
-ggplot(nmds_data, aes(x=MDS1, y=MDS2, shape = Researcher_Reactor, color = Day)) +
-  geom_text(aes(label=Day)) +
-  geom_point()
-
-ggplot()+
-  geom_text(data=nmds_data, aes(x=MDS1,y=MDS2,label=Day,color=Researcher), size=5) +
-  geom_polygon(data=nmds_data, aes(x=MDS1,y=MDS2,fill=Researcher, group=Researcher), alpha = 0.2) +
-  coord_equal() +
-  theme_bw() +
-  scale_color_manual(values = c("firebrick2", "chocolate1",
-                                "green4", "mediumblue")) +
-  labs(title = "NMDS Plot for all Representative Genomes") +
-  theme(panel.background = element_blank(),
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        plot.background = element_blank(),
-        plot.title = element_text())
-
 ggplot()+
   geom_text(data=nmds_data, aes(x=MDS1,y=MDS2,label=Day,color=Researcher_Reactor), size=4, nudge_x=0.1) +
   geom_point(data=nmds_data, aes(x=MDS1,y=MDS2,color=Researcher_Reactor), size=2) +
@@ -68,12 +52,11 @@ ggplot()+
   coord_equal() +
   theme_bw() +
   scale_color_manual(values = c("red", "black", "chocolate1", "orangered1",
-                              "green", "green4", "cornflowerblue", "mediumblue",
-                              "blueviolet", "darkorchid1", "violetred")) +
+                                "green", "green4", "cornflowerblue", "mediumblue",
+                                "blueviolet", "darkorchid1", "violetred")) +
   labs(title = "NMDS Plot for all Representative Genomes - Reactor Separated") +
   theme(panel.background = element_blank(),
         panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
         plot.background = element_blank(),
         plot.title = element_text())
-
